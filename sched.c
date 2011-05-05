07507b4a1d3bd038ddcd0cb4040f511f9a41be2a
Daniel Hathorn <dmhathorn@gmail.com>
2011-05-05 10:45:48 -0600

    tweeking mrq

--- sched.c
+++ sched.c
@@ -5443,7 +5443,7 @@ pick_next_task(struct rq *rq)
 		return p;
 	} */
 	int s =0;
-	int loc;
+	int loc = 0;
 	if (likely(rq->nr_running == rq->cfs.nr_running)) {
 //		p = fair_sched_class.pick_next_task(rq);
 		struct cfs_rq *cfs_rq;
